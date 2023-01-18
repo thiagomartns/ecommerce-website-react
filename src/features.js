f4f3ea35fd4332d4a0c963a2f35c31b10c8671da
thiago <tmg3.mn@gmail.com>
2023-01-18 11:43:18 -0300

    Objeto 'features' adicionado

---
+++ src/features.js
@@ -0,0 +1,26 @@
+export const features = [
+  {
+    id: 1,
+    img: './assets/img/shipping.png',
+    title: "FREE SHIPPING",
+    desc: 'Free worldwide shipping on all orders.'
+  },
+  {
+    id: 2,
+    img: './assets/img/return.png',
+    title: "30 DAYS RETURN",
+    desc: 'No question return and easy refund in 14 days'
+  },
+  {
+    id: 3,
+    img: './assets/img/gift.png',
+    title: "GIFT CARDS",
+    desc: 'Buy gift cards and use coupon codes easily.'
+  },
+  {
+    id: 4,
+    img: './assets/img/contact.png',
+    title: "CONTACT US!",
+    desc: 'Keep in touch via email and support system.'
+  }
+]
