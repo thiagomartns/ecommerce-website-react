2ef166065688fc110132ec6510f4ef3fbfcba2e6
thiago <tmg3.mn@gmail.com>
2023-01-18 09:24:22 -0300

    Componente 'MenuNav' adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import MenuNav from "./Components/menuNav/MenuNav";
 
 
 function App() {
   return (
     <div className="App">
-
+      <MenuNav />
     </div>
   );
 }
---
+++ src/Components/menuNav/MenuNav.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './menuNav.scss'
+import { products } from '../../products'
+
+const MenuNav = () => {
+  return (
+    <div>
+      <nav id="nav">
+        <div className="navTop">
+          <div className="navItem">
+            <img src="./assets/img/sneakers.png" alt="" />
+          </div>
+          <div className="navItem">
+            <div className="search">
+              <input type="text" placeholder="Search..." className="searchInput" />
+              <img
+                src="./assets/img/search.png"
+                width="20"
+                height="20"
+                alt=""
+                className="searchIcon"
+              />
+            </div>
+          </div>
+          <div className="navItem">
+            <span className="limitedOffer">Limited Offer!</span>
+          </div>
+        </div>
+        <div className="navBottom">
+          {products.map((product) => (
+            <h3 className="menuItem" key={product.id}>
+              {product.title.toUpperCase()}
+            </h3>
+          ))}
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default MenuNav
