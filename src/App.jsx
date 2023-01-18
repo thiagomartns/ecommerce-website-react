4138d73e81884c6369a59b37525bb16c9a6cd5cd
thiago <tmg3.mn@gmail.com>
2023-01-18 13:41:53 -0300

    Componente 'Product' adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Features from "./Components/features/Features";
 import MenuNav from "./Components/menuNav/MenuNav";
+import Product from "./Components/product/Product";
 import Slider from "./Components/slider/Slider";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <MenuNav handleClick={handleClick} />
       <Slider style={style} />
       <Features />
+      <Product />
     </div>
   );
 }
