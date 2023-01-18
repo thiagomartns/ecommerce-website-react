7745e5320d7379ef10b168529a9f1652286a49ba
thiago <tmg3.mn@gmail.com>
2023-01-18 08:34:31 -0300

    init

---
+++ src/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./assets/img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./assets/img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./assets/img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./assets/img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./assets/img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./assets/img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./assets/img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./assets/img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./assets/img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./assets/img/hippie2.png",
+      },
+    ],
+  },
+];
