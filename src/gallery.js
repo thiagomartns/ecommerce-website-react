c5f1fd2e29eaea63b208c5276c501ed2c467a6a7
thiago <tmg3.mn@gmail.com>
2023-01-19 10:15:25 -0300

    Objeto 'gallery' adicionado

---
+++ src/gallery.js
@@ -0,0 +1,20 @@
+export const gallery = [
+  {
+    id: 1,
+    title: 'Be Yourself!',
+    img: 'https://images.pexels.com/photos/9295809/pexels-photo-9295809.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    order: 0
+  },
+  {
+    id: 2,
+    title: 'This is the First Day of Your New Life',
+    img: 'https://images.pexels.com/photos/1040427/pexels-photo-1040427.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    order: 1
+  },
+  {
+    id: 3,
+    title: 'Just Do it!',
+    img: 'https://images.pexels.com/photos/7856965/pexels-photo-7856965.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    order: 0
+  }
+]
